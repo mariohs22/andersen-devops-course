30ea716419fb1af18315ced1efe98ddbb828fd65
Siarhei Burakouski <serge.burakovsky@gmail.com>
2021-06-11 16:08:48 +0300

    task3: done

--- task3_telegram_bot/git/git.go
+++ task3_telegram_bot/git/git.go
@@ -1,5 +1,12 @@
 package main
 
+// Template package for debug:
+// Use: cd git
+//      go run .
+//
+// Deploy: env GOOS=linux go build -o bin/webhook main.go
+//         serverless deploy -v
+
 import (
 	"context"
 	"fmt"
@@ -11,25 +18,60 @@ import (
 
 const GITHUB_ACCOUNT = "mariohs22"
 const GITHUB_REPOSITORY = "andersen-devops-course"
+const TASK_LABEL = "task"
 
 func main() {
-	req := "2"
+	req := "/task3"
 
-	client := github.NewClient(nil)
-	ctx := context.Background()
-	opt := &github.IssueListByRepoOptions{State: "all", Labels: []string{"task"}}
-	listIssues, _, _ := client.Issues.ListByRepo(ctx, GITHUB_ACCOUNT, GITHUB_REPOSITORY, opt)
+	gitTemplate := regexp.MustCompile(`(?i)^/git$`)
+	helpTemplate := regexp.MustCompile(`(?i)^/help$`)
+	tasksTemplate := regexp.MustCompile(`(?i)^/tasks$`)
+	taskNTemplate := regexp.MustCompile(`(?i)^/task(\d+)$`)
 
-	rg := fmt.Sprintf("Task %v:", req)
-	fmt.Println(rg)
-	captionTemplate := regexp.MustCompile(`"Task (\d?): (.*?)"`)
-	for i, task := range listIssues {
-		title := github.Stringify(task.Title)
-		titleParsed := captionTemplate.FindStringSubmatch(title)
-		if titleParsed[1] == req {
-			caption := strings.ToLower(strings.Replace(titleParsed[2], " ", "_", -1))
-			url := fmt.Sprintf("https://github.com/%v/%v/tree/main/task%v_%v", GITHUB_ACCOUNT, GITHUB_REPOSITORY, titleParsed[1], caption)
-			fmt.Printf("%v. %v %v\n", i+1, title, url)
+	if helpTemplate.MatchString(req) {
+		fmt.Println(`Use this commands: ...`)
+	} else if gitTemplate.MatchString(req) {
+		url := fmt.Sprintf("https://github.com/%v/%v", GITHUB_ACCOUNT, GITHUB_REPOSITORY)
+		fmt.Println(url)
+	} else if tasksTemplate.MatchString(req) {
+		client := github.NewClient(nil)
+		ctx := context.Background()
+		opt := &github.IssueListByRepoOptions{State: "closed", Labels: []string{TASK_LABEL}}
+		listTasks, _, _ := client.Issues.ListByRepo(ctx, GITHUB_ACCOUNT, GITHUB_REPOSITORY, opt)
+		found := false
+		for i, task := range listTasks {
+			found = true
+			message := fmt.Sprintf("%v. %v\n", i+1, github.Stringify(task.Title))
+			fmt.Println(message)
 		}
+		if !found {
+			fmt.Println("Done tasks not found")
+		}
+
+	} else if taskNTemplate.MatchString(req) {
+		taskNumber := taskNTemplate.FindStringSubmatch(req)[1]
+		client := github.NewClient(nil)
+		ctx := context.Background()
+		opt := &github.IssueListByRepoOptions{State: "all", Labels: []string{TASK_LABEL}}
+		listTasks, _, _ := client.Issues.ListByRepo(ctx, GITHUB_ACCOUNT, GITHUB_REPOSITORY, opt)
+		captionTemplate := regexp.MustCompile(`"Task (\d?): (.*?)"`)
+		found := false
+		for i, task := range listTasks {
+			title := github.Stringify(task.Title)
+			titleParsed := captionTemplate.FindStringSubmatch(title)
+			if titleParsed[1] == taskNumber {
+				found = true
+				caption := strings.ToLower(strings.Replace(titleParsed[2], " ", "_", -1))
+				url := fmt.Sprintf("https://github.com/%v/%v/tree/main/task%v_%v", GITHUB_ACCOUNT, GITHUB_REPOSITORY, titleParsed[1], caption)
+				fmt.Printf("%v. %v %v\n", i+1, title, url)
+			}
+		}
+		if !found {
+			fmt.Printf("Task #%v is not found", taskNumber)
+		}
+
+	} else {
+		fmt.Println("Wrong command, use /help for help")
 	}
+
 }
--- task3_telegram_bot/main.go
+++ task3_telegram_bot/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -15,6 +16,7 @@ import (
 
 const GITHUB_ACCOUNT = "mariohs22"
 const GITHUB_REPOSITORY = "andersen-devops-course"
+const TASK_LABEL = "task"
 
 func main() {
 	b, err := tb.NewBot(tb.Settings{
@@ -26,29 +28,70 @@ func main() {
 	}
 
 	b.Handle(tb.OnText, func(m *tb.Message) {
-		// git := regexp.MustCompile("/^/git$/i")
-		// if git.MatchString(m.Text) {
+		startTemplate := regexp.MustCompile(`(?i)^/start$`)
+		gitTemplate := regexp.MustCompile(`(?i)^/git$`)
+		helpTemplate := regexp.MustCompile(`(?i)^/help$`)
+		tasksTemplate := regexp.MustCompile(`(?i)^/tasks$`)
+		taskNTemplate := regexp.MustCompile(`(?i)^/task(\d+)$`)
 
-		if strings.EqualFold(m.Text, "/git") {
+		client := github.NewClient(nil)
+		ctx := context.Background()
 
-			b.Send(m.Chat, "https://github.com/"+GITHUB_ACCOUNT+"/"+GITHUB_REPOSITORY)
+		if startTemplate.MatchString(m.Text) {
+			b.Send(m.Chat, "Welcome to telegram bot of my Andersen DevOps Courses repository. Type /help for help")
 
-		} else if strings.EqualFold(m.Text, "/tasks") {
+		} else if helpTemplate.MatchString(m.Text) {
+			b.Send(m.Chat, `To communicate with bot, use these commands please:
+/help  - display this message
+/git   - show link to my course repository
+/tasks - show list of completed tasks
+/task# - show link to specifeed task, where # is a task number`)
 
-			b.Send(m.Chat, "List of completed tasks:\n")
-			client := github.NewClient(nil)
-			ctx := context.Background()
-			opt := &github.IssueListByRepoOptions{State: "closed", Labels: []string{"task"}}
-			listIssues, _, _ := client.Issues.ListByRepo(ctx, GITHUB_ACCOUNT, GITHUB_REPOSITORY, opt)
-			for i, task := range listIssues {
+		} else if gitTemplate.MatchString(m.Text) {
+			url := fmt.Sprintf("https://github.com/%v/%v", GITHUB_ACCOUNT, GITHUB_REPOSITORY)
+			b.Send(m.Chat, url)
+
+		} else if tasksTemplate.MatchString(m.Text) {
+			b.Send(m.Chat, "The list of completed tasks:")
+			opt := &github.IssueListByRepoOptions{State: "closed", Labels: []string{TASK_LABEL}}
+			listTasks, _, _ := client.Issues.ListByRepo(ctx, GITHUB_ACCOUNT, GITHUB_REPOSITORY, opt)
+			found := false
+			for i, task := range listTasks {
+				found = true
 				message := fmt.Sprintf("%v. %v\n", i+1, github.Stringify(task.Title))
 				b.Send(m.Chat, message)
 			}
+			if !found {
+				b.Send(m.Chat, "Unfortunately, completed tasks is not found")
+			}
+
+		} else if taskNTemplate.MatchString(m.Text) {
+			taskNumber := taskNTemplate.FindStringSubmatch(m.Text)[1]
+			opt := &github.IssueListByRepoOptions{State: "all", Labels: []string{TASK_LABEL}}
+			listTasks, _, _ := client.Issues.ListByRepo(ctx, GITHUB_ACCOUNT, GITHUB_REPOSITORY, opt)
+			captionTemplate := regexp.MustCompile(`"Task (\d?): (.*?)"`)
+			found := false
+			for _, task := range listTasks {
+				title := github.Stringify(task.Title)
+				titleParsed := captionTemplate.FindStringSubmatch(title)
+				if titleParsed[1] == taskNumber {
+					found = true
+					status := "NOT COMPLETED"
+					if github.Stringify(task.State) == `"closed"` {
+						status = "COMPLETED"
+					}
+					caption := strings.ToLower(strings.Replace(titleParsed[2], " ", "_", -1))
+					url := fmt.Sprintf("https://github.com/%v/%v/tree/main/task%v_%v", GITHUB_ACCOUNT, GITHUB_REPOSITORY, titleParsed[1], caption)
+					b.Send(m.Chat, fmt.Sprintf("%v [%v] %v\n", title, status, url))
+				}
+			}
+			if !found {
+				b.Send(m.Chat, "Task #%v is not found", taskNumber)
+			}
 
 		} else {
-			b.Send(m.Chat, m.Text)
+			b.Send(m.Chat, "Unknown command, use /help for help")
 		}
-
 	})
 
 	lambda.Start(func(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
