edf1929ac142d6925b5e3daecbb989617d232413
mariohs22 <serge.burakovsky@gmail.com>
2021-06-29 18:01:59 +0300

    task6: writing terraform files...
    temporary commit to save work state

--- task6_aws/variables.tf
+++ task6_aws/variables.tf
@@ -1,3 +1,15 @@
+variable "AWS_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-1"
+}
+
+variable "EC2_instances_ami" {
+  description = "AMI for EC2 instances for us-west-1 region"
+  type        = string
+  default     = "ami-02f24ad9a1d24a799"
+}
+
 variable "EC2_instance1_name" {
   description = "Value of the Name tag for the EC2 instance"
   type        = string
