6d93ccd5a613bd61b831981b9cd056b10a3b9130
mariohs22 <serge.burakovsky@gmail.com>
2021-06-29 11:17:15 +0300

    task6: update readme file:
    Add Prerequisites section

---
+++ task6_aws/variables.tf
@@ -0,0 +1,11 @@
+variable "EC2_instance1_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "EC2_Instance1"
+}
+
+variable "EC2_instance2_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "EC2_Instance2"
+}
